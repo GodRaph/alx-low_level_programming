4d75514f0347e5c7941d4515d6676b396cae28ab
GodRaph <godraphodion@gmail.com>
2023-09-11 18:07:42 -0700

    Prinitng alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabets lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+		putchar('\n);
+		return (0);
+}
