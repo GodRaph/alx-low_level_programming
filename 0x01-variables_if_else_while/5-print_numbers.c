90f4e33f95f83dc5cc7337cdf7304a04f6a7337b
GodRaph <godraphodion@gmail.com>
2023-09-11 18:49:56 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program that print all single digits with certain conditions
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
